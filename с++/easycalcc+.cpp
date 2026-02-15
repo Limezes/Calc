49db0a6e3c938a14f98644fb7db2c32738c0a20d
✞Lime✞ <z6nqhtw7nm@privaterelay.appleid.com>
2026-02-15 20:10:39 +0300

    Update easycalcc+.cpp

--- с++/easycalcc+.cpp
+++ с++/easycalcc+.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+#include <limits>
+using namespace std;
 
+int main() {
+    double num1, num2;
+    char operation;
+    
+    cout << "=== ПРОСТОЙ КАЛЬКУЛЯТОР ===" << endl;
+    cout << "Введите первое число: ";
+    cin >> num1;
+    
+    cout << "Введите оператор (+, -, *, /): ";
+    cin >> operation;
+    
+    cout << "Введите второе число: ";
+    cin >> num2;
+    
+    double result;
+    bool validOperation = true;
+    
+    switch(operation) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+            } else {
+                cout << "Ошибка: деление на ноль!" << endl;
+                validOperation = false;
+            }
+            break;
+        default:
+            cout << "Ошибка: неверный оператор!" << endl;
+            validOperation = false;
+    }
+    
+    if (validOperation) {
+        cout << "Результат: " << num1 << " " << operation << " " << num2 << " = " << result << endl;
+    }
+    
+    return 0;
+}
